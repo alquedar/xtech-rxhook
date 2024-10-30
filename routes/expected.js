9b20bf41a8e315f29db62b5debe461e1114c4b0c
alquedar <alquedar@gmail.com>
2024-10-30 14:24:09 +0700

    Update expected.js

--- routes/expected.js
+++ routes/expected.js
@@ -3,77 +3,80 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
   console.log(req.body);
-  res.send('send a query string to get a expected response');
+  res.json({
+    status: 200,
+    message: 'send a query string to get a expected response',
+  });
 });
 
 router.get('/200', function (req, res, next) {
   console.log(req.body);
-  res.status(200).send('OK');
+  return res.status(200).json({ status: 200, message: 'OK' });
 });
 
 router.get('/202', function (req, res, next) {
   console.log(req.body);
-  res.status(202).send('Accepted');
+  res.status(202).json({ status: 200, message: 'Accepted' });
 });
 
 router.get('/500', function (req, res, next) {
   console.log(req.body);
-  res.status(500).send('Internal Server Error');
+  res.status(500).json({ status: 200, message: 'Internal Server Error' });
 });
 
 router.get('/400', function (req, res, next) {
   console.log(req.body);
-  res.status(400).send('Bad Request');
+  res.status(400).json({ status: 200, message: 'Bad Request' });
 });
 
 router.get('/401', function (req, res, next) {
   console.log(req.body);
-  res.status(401).send('Unauthorized');
+  res.status(401).json({ status: 200, message: 'Unauthorized' });
 });
 
 router.get('/403', function (req, res, next) {
   console.log(req.body);
-  res.status(403).send('Forbidden');
+  res.status(403).json({ status: 200, message: 'Forbidden' });
 });
 
 router.get('/404', function (req, res, next) {
   console.log(req.body);
-  res.status(404).send('Not Found');
+  res.status(404).json({ status: 200, message: 'Not Found' });
 });
 
 router.get('/405', function (req, res, next) {
   console.log(req.body);
-  res.status(405).send('Method Not Allowed');
+  res.status(405).json({ status: 200, message: 'Method Not Allowed' });
 });
 
 router.get('/406', function (req, res, next) {
   console.log(req.body);
-  res.status(406).send('Not Acceptable');
+  res.status(406).json({ status: 200, message: 'Not Acceptable' });
 });
 
 router.get('/408', function (req, res, next) {
   console.log(req.body);
-  res.status(408).send('Request Timeout');
+  res.status(408).json({ status: 200, message: 'Request Timeout' });
 });
 
 router.get('/409', function (req, res, next) {
   console.log(req.body);
-  res.status(409).send('Conflict');
+  res.status(409).json({ status: 200, message: 'Conflict' });
 });
 
 router.get('/410', function (req, res, next) {
   console.log(req.body);
-  res.status(410).send('Gone');
+  res.status(410).json({ status: 200, message: 'Gone' });
 });
 
 router.get('/411', function (req, res, next) {
   console.log(req.body);
-  res.status(411).send('Length Required');
+  res.status(411).json({ status: 200, message: 'Length Required' });
 });
 
 router.get('/412', function (req, res, next) {
   console.log(req.body);
-  res.status(412).send('Precondition Failed');
+  res.status(412).json({ status: 200, message: 'Precondition Failed' });
 });
 
 module.exports = router;
